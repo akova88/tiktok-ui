8c525fe51c3a5f926278f0b98b04d5338a9abb80
akova88 <phuoc.fiit@gmail.com>
2023-09-08 09:27:44 +0700

    finish layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Header from '../components/Header';
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
